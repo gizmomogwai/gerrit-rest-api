2146b3f3807ea8964fc26398fe1cf46bb0b4ace1
Christian Koestlin <christian.koestlin@esrlabs.com>
2021-04-23 04:04:13 +0200

    Implement list command

--- source/app.d
+++ source/app.d
@@ -6,6 +6,12 @@ import mir.ion.deser.json;
 struct Config
 {
     Server[] servers;
+    User[] users;
+}
+
+struct User {
+    string nickName;
+    string userName;
 }
 
 struct Server
@@ -22,9 +28,11 @@ Server getServerState(Server server, string user)
 {
     auto request = new Request;
     request.authenticator = new BasicAuthentication(server.userName, server.password);
+    request.verbosity = 0;
     auto p = queryParams("q", "status:open owner:%s".format(user));
     auto response = request.get("%s/a/changes/".format(server.url), p);
-    auto responseString = response.responseBody.toString;
+    auto responseString = response.responseBody.to!string;
+    // see https://gerrit-review.googlesource.com/Documentation/rest-api.html#output why stripping is needed
     auto json = parseJSON(responseString[5 .. $ - 1]);
     server.openIssues = json.array.length;
     return server;
@@ -33,32 +41,36 @@ Server getServerState(Server server, string user)
 void usage(string executable)
 {
     throw new Exception(
-            "Usage: %s config (review|open) username".format(executable));
+            "Usage: %s config (review username|open user|list)".format(executable));
+}
+
+string stateForUser(Server[] servers, string user) {
+    return servers
+        .map!(server => getServerState(server, user))
+        .map!((server) {
+                auto result = "%s:%s".format(server.nickName, server.openIssues);
+                return (server.openIssues == 0 ? result.green : result.red).to!string;
+            })
+        .join(" | ");
 }
 
 void main(string[] args)
 {
     auto executable = args[0];
-    if (args.length != 4)
-    {
-        usage(executable);
-    }
 
-    auto config = args[1];
+    auto configFile = args[1];
     auto command = args[2];
-    auto user = args[3];
-    auto servers = deserializeJson!(Config)(config.readText).servers;
+    auto config = deserializeJson!(Config)(configFile.readText);
+    auto servers = config.servers;
+    auto users = config.users;
     switch (command)
     {
     case "review":
-        writeln(servers.map!(server => getServerState(server, user))
-                .map!((server) {
-                    auto result = "%s:%s".format(server.nickName, server.openIssues);
-                    return (server.openIssues == 0 ? result.green : result.red).to!string;
-                })
-                .join(" | "));
+        auto user = args[3];
+        writeln(servers.stateForUser(user));
         break;
     case "open":
+        auto user = args[3];
         foreach (server; servers.map!(server => getServerState(server, user)))
         {
             if (server.openIssues > 0)
@@ -67,6 +79,11 @@ void main(string[] args)
                 writeln("open ", url);
                 std.process.execute(["open", url]);
             }
+        }
+        break;
+    case "list":
+        foreach (user; users) {
+            writeln(user.nickName, ": ", servers.stateForUser(user.userName));
         }
         break;
     default:
